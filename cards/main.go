ae555a47719ad326e2973bbd69911ccf36be2f3e
tech1ndex <50547519+tech1ndex@users.noreply.github.com>
2022-07-11 16:19:34 -0400

    added testing branch

--- cards/main.go
+++ cards/main.go
@@ -8,6 +8,16 @@ func main() {
 	// var card string = "Ace of Spades"
 
 	//Short Version of Declaration
-	card := "Ace of Spades"
+	//card := "Ace of Spades"
+
+	//Note that := is only for NEW Variables, re-declaration doesnt need it
+	card := newCard()
+
+	//Code to print line to screen
 	fmt.Println(card)
+
+}
+
+func newCard() string {
+	return "Five of Diamonds"
 }
---
+++ testing/main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+var deckSize int
+
+func main() {
+	deckSize = 52
+	fmt.Println(deckSize)
+}
