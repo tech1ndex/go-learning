4f842dbda73f4e0655e2e663db25ca0afd167a93
Victor Bajada <bajada.victor@gmail.com>
2022-08-17 11:56:57 -0400

    added comments

--- quiz/main.go
+++ quiz/main.go
@@ -5,12 +5,15 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"strings"
 )
 
+// Main Function that will read in answers and compare them to values in CSV file
 func main() {
 	csvFilename := flag.String("csv", "questions.csv", "a csv file in the format of 'question,answer'")
 	flag.Parse()
 
+	// Use the OS Package to attempt to open file in same directory and error if file cannot be found
 	file, err := os.Open(*csvFilename)
 	if err != nil {
 		exit(fmt.Sprintf("Failed to open the CSV file %s\n", *csvFilename))
@@ -20,8 +23,11 @@ func main() {
 	if err != nil {
 		exit("Failed to parse the provided CSV file.")
 	}
+
+	// Iterare through CSV file and parse lines as questions to be stored in 'questions' slice
 	questions := parseLines(lines)
 
+	// Declare correct slice, iterate through questions and read answer based on user input
 	correct := 0
 	for i, qu := range questions {
 		fmt.Printf("Question #%d: %s = \n", i+1, qu.q)
@@ -34,12 +40,13 @@ func main() {
 	fmt.Printf("You scored %d out of %d.\n", correct, len(questions))
 }
 
+// Function to parse questions
 func parseLines(lines [][]string) []question {
 	ret := make([]question, len(lines))
 	for i, line := range lines {
 		ret[i] = question{
 			q: line[0],
-			a: line[1],
+			a: strings.TrimSpace(line[1]),
 		}
 	}
 	return ret
