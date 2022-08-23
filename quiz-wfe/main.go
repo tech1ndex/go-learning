957b51289d938b737fb29d299e5303f5a0d5ca6a
Victor Bajada <bajada.victor@gmail.com>
2022-08-23 19:03:47 +0000

    added comments and serving CSS

--- quiz-wfe/main.go
+++ quiz-wfe/main.go
@@ -13,12 +13,19 @@ type Quiz struct {
 
 func main() {
 	tmpl := template.Must(template.ParseFiles("template.html"))
+
+	//Add code to serve CSS
+	http.Handle("/static/",
+		http.StripPrefix("/static",
+			http.FileServer(http.Dir("static"))))
+	//Serve Site based on Data
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		data := Quiz{
 			QuizName: "ACE Quiz",
 		}
 		tmpl.Execute(w, data)
 	})
+	//Print Message to Indicate we are Serving
 	fmt.Println("Serving on port 8080 :)")
 	http.ListenAndServe(":8080", nil)
 }
