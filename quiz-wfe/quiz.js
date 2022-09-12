b143e617a759dbc31a1feb0af11dd6da9bbe40e3
Victor Bajada <bajada.victor@gmail.com>
2022-09-12 17:39:55 +0000

    fixed closing brackets

--- quiz-wfe/quiz.js
+++ quiz-wfe/quiz.js
@@ -60,5 +60,30 @@ getNewQuestion = () => {
     })
 
     availableQuestions.splice(questionsIndex, 1)
-    
+    acceptingAnswers = true
 }
+
+choices.forEach(choice => {
+    choice.addEventListener('click', e => {
+        if(!acceptingAnswers) return
+
+        acceptingAnswers = false
+        const selectedChoice = e.target
+        const selectedAnswer = selectedChoice.dataset['number']
+
+        let classToApply = selectedAnswer == currentQuestion.answer ? 'correct' :
+        'incorrect'
+
+        if(classToApply === 'correct'){
+            incrementScore(SCORE_POINTS)
+        }
+
+        selectedChoice.parentElement.classList.add(classToApply)
+
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply)
+            getNewQuestion()
+        
+        }, 1000)
+    })
+})
