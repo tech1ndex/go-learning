95d7ad142ac1cd7426beba25d13ab76c2afcc867
tech1ndex <50547519+tech1ndex@users.noreply.github.com>
2022-09-13 22:13:24 -0400

    updated main.go

---
+++ quiz-wfe/main_old.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+)
+
+type Quiz struct {
+	QuizName string
+	Question string
+}
+
+func main() {
+	tmpl := template.Must(template.ParseFiles("index.html", "quiz.html"))
+
+	//Add code to serve CSS
+	http.Handle("/static/",
+		http.StripPrefix("/static",
+			http.FileServer(http.Dir("static"))))
+
+	//Serve Site based on Data
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := Quiz{
+			QuizName: "ACE Quiz",
+		}
+		tmpl.Execute(w, data)
+	})
+
+	//Actually Listen on Port 8080
+	http.ListenAndServe(":8080", nil)
+}
