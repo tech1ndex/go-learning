0ad632a265e90141160068e026f0dfc05ea4efaa
Victor Bajada <bajada.victor@gmail.com>
2022-09-15 17:26:17 +0000

    updated filepaths

--- quiz-wfe/main.go
+++ quiz-wfe/main.go
@@ -1,67 +1,32 @@
 package main
 
 import (
-	"embed"
 	"html/template"
-	"io/fs"
 	"net/http"
 )
 
-const (
-	layoutsDir   = "templates/layouts"
-	templatesDir = "templates"
-	extension    = "/*.html"
-)
-
-type Quiz struct {
-	QuizName string
-	Question string
+type Todo struct {
+	Title string
+	Done  bool
 }
 
-var (
-	//go:embed templates/* templates/layouts/*
-	files     embed.FS
-	templates map[string]*template.Template
-)
-
-func LoadTemplates() error {
-	if templates == nil {
-		templates = make(map[string]*template.Template)
-	}
-	tmplFiles, err := fs.ReadDir(files, templatesDir)
-	if err != nil {
-		return err
-	}
-
-	for _, tmpl := range tmplFiles {
-		if tmpl.IsDir() {
-			continue
-		}
-
-		pt, err := template.ParseFS(files, templatesDir+"/"+tmpl.Name(), layoutsDir+extension)
-		if err != nil {
-			return err
-		}
-
-		templates[tmpl.Name()] = pt
-	}
-	return nil
+type TodoPageData struct {
+	PageTitle string
+	Todos     []Todo
 }
 
-const index = "index.html"
-
-func indexLoad(w http.ResponseWriter, r *http.Request) {
-	t, ok := templates[index]
-	if !ok {
-		// TODO handle error
-		return
-	}
-
-	data := Quiz{
-		QuizName: "ACE Quiz",
-	}
-
-	if err := t.Execute(w, data); err != nil {
-		// TODO handle error
-	}
+func main() {
+	tmpl := template.Must(template.ParseFiles("index.html"))
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		data := TodoPageData{
+			PageTitle: "My TODO list",
+			Todos: []Todo{
+				{Title: "Build WFE for Quiz", Done: false},
+				{Title: "Create Quiz Application", Done: true},
+				{Title: "Create Web application", Done: false},
+			},
+		}
+		tmpl.Execute(w, data)
+	})
+	http.ListenAndServe(":8080", nil)
 }
