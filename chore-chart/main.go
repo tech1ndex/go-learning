8683e10cdadeaa78227c640bcd31aeb6a066ce11
tech1ndex <50547519+tech1ndex@users.noreply.github.com>
2022-12-09 12:07:01 -0500

    updated

--- chore-chart/main.go
+++ chore-chart/main.go
@@ -6,7 +6,13 @@ import (
 
 	"github.com/gorilla/mux"
 	log "github.com/sirupsen/logrus"
+	_ "github.com/go-sql-driver/mysql"
+	 "github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
 )
+
+//Connect to MYSQL Database
+var db, _ := gorm.Open("mysql", "root:root@/chores?charset=utf8&parseTime=True&loc=Local")
 
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	log.Info("API Health is OK")
