55d8368f1e2ebc246fb80daa2978150b56863538
tech1ndex <50547519+tech1ndex@users.noreply.github.com>
2022-12-10 10:45:11 -0500

    updated to always print message

--- healthcheck/main.go
+++ healthcheck/main.go
@@ -12,7 +12,7 @@ func main() {
 	}
 
 	_, err := client.Head("https://hc.tech1ndex.ca/ping/d3603d5e-ba64-4e26-b866-65e1a07d02a6")
-	if err != nil {
-		fmt.Printf("%s", err)
-	}
+
+	fmt.Printf("%s", err)
+
 }
